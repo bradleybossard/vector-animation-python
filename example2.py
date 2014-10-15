77b78558433bd99b9d688df60fa1a8532adba373
Bradley Bossard <bradleybossard@gmail.com>
2014-10-15 09:55:31 -0700

    Adding example2

---
+++ example2.py
@@ -0,0 +1,23 @@
+import numpy as np
+import gizeh
+import moviepy.editor as mpy
+
+W,H = 128,128
+duration = 2
+ncircles = 20 # Number of circles
+
+def make_frame(t):
+
+    surface = gizeh.Surface(W,H)
+
+    for i in range(ncircles):
+        angle = 2*np.pi*(1.0*i/ncircles+t/duration)
+        center = W*( 0.5+ gizeh.polar2cart(0.1,angle))
+        circle = gizeh.circle(r= W*(1.0-1.0*i/ncircles),
+                              xy= center, fill= (i%2,i%2,i%2))
+        circle.draw(surface)
+
+    return surface.get_npimage()
+
+clip = mpy.VideoClip(make_frame, duration=duration)
+clip.write_gif("example2.gif",fps=15, opt="OptimizePlus", fuzz=10)
