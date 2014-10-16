5fd3b711450a27714214ae76342266efc3143959
Bradley Bossard <bradleybossard@gmail.com>
2014-10-16 09:24:33 -0700

    Adding the last two examples, but they current don't work, asking the author why

---
+++ example11.py
@@ -0,0 +1,27 @@
+import moviepy.editor as mpy
+import numpy as np
+import gizeh as gz
+
+clip = mpy.VideoFileClip("example10.gif")
+(w, h), d = clip.size, clip.duration
+center=  np.array([w/2, h/2])
+
+def my_filter(get_frame, t):
+    """ Transforms a frame (given by get_frame(t)) into a different
+    frame, using vector graphics."""
+
+    surface = gz.Surface(w,h)
+    fill = (gz.ImagePattern(get_frame(t), pixel_zero=center).scale(1.5, center=center))
+    #fill = (gz.ImagePattern(get_frame(t), pixel_zero=center))
+    #pattern = gz.ImagePattern(get_frame(t), pixel_zero=center)
+    #fill = pattern.scale(1.5, None, center=center)
+    for (nfaces,angle,f) in ([3, 0, 1.0/6],
+                              [5, np.pi/3, 3.0/6],
+                              [7, 2*np.pi/3, 5.0/6]):
+        xy = (f*w, h*(.5+ .05*np.sin(2*np.pi*(t/d+f))))
+        shape = gz.regular_polygon(w/6,nfaces, xy = xy,
+                fill=fill.rotate(angle, center))
+        shape.draw(surface)
+    return surface.get_npimage()
+
+clip.fl(my_filter).write_gif("example11.gif")
---
+++ example12.py
@@ -0,0 +1,32 @@
+import gizeh as gz
+import moviepy.editor as mpy
+import numpy as np
+
+def add_zoom(clip, target_center, zoom_center, zoom_radius, zoomx):
+
+    w, h = clip.size
+
+    def fl(im):
+        """ transforms the image by adding a zoom """
+
+        surface = gz.Surface.from_image(im)
+        fill = gz.ImagePattern(im, pixel_zero=target_center,
+                               filter='best')
+        line = gz.polyline([target_center, zoom_center],
+                           stroke_width=3)
+        circle_target= gz.circle(zoom_radius, xy=target_center,
+                                 fill=fill, stroke_width=2)
+        circle_zoom = gz.circle(zoom_radius, xy=zoom_center, fill=fill,
+                       stroke_width=2).scale(zoomx, center=zoom_center)
+        for e in line, circle_zoom, circle_target:
+            e.draw(surface)
+        return surface.get_npimage()
+
+    return clip.fl_image(fl)
+
+
+clip = mpy.VideoFileClip("example10.gif")
+w, h = clip.size
+clip_with_zoom = clip.fx(add_zoom, target_center = [w/2, h/3], zoomx=3,
+                   zoom_center = [5*w/6, h/4], zoom_radius=15)
+clip_with_zoom.write_gif("example12.gif")
