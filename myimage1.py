154738e468edd6bb6b9404fa8497f7620103fe96
Bradley Bossard <bradleybossard@gmail.com>
2014-11-06 15:16:29 -0800

    Adding multiple particles, random colors, directions, and easing functions to orbit animation

--- myimage1.py
+++ myimage1.py
@@ -1,5 +1,7 @@
 import math
+import random
 import numpy as np
+import pytweening
 import gizeh
 import moviepy.editor as mpy
 
@@ -7,8 +9,18 @@ import moviepy.editor as mpy
 W,H = 200,200 # width, height, in pixels
 duration = 2 # duration of the clip, in seconds
 radius1 = W / 2 - 10
-radius2 = radius1 - 2
 radius3 = 4
+
+# Array containing instances of Star class
+particles = []
+numParticles = 10
+
+class Particle():
+  x = 0.0
+  y = 0.0
+  radius = 0
+  #vecX = 0.0
+  #vecY = 0.0
 
 
 def supersample(clip, d, nframes):
@@ -20,43 +32,68 @@ def supersample(clip, d, nframes):
         return avg.astype("uint8")
     return clip.fl(fl)
 
+# Initialize particles
+def init():
+  for i in range(numParticles):
+    particle = Particle()
+    particle.direction = random.choice([-1, 1])
+    particle.radius = random.uniform(0.1, 2.5)
+    particle.orbit_radius = random.uniform(1, H/2)
+    particle.color = [random.random(), random.random(), random.random()]
+    particle.easing = random.choice([1, 2, 3])
+    particles.append(particle)
+
+
 def make_frame(t):
-    colorDiv = t + 0.01
-    color1= [1.0 / colorDiv, 0.0, 0.0]
-    color2 = [0.0, 1.0 / colorDiv, 0.0]
+    #colorDiv = t + 0.01
+    #color1= [1.0 / colorDiv, 0.0, 0.0]
+    #color2 = [0.0, 1.0 / colorDiv, 0.0]
 
-    angle = (duration - t)/duration * 360
-    radians = math.radians(angle)
-    cosR = math.cos(radians)
-    sinR = math.sin(radians)
-    gradient= gizeh.ColorGradient("linear",((0,(0,.5,1)),(1,(0,1,1))), xy1=(-cosR,-sinR), xy2=(cosR,sinR))
-    #gradient=color2
+    #gradient= gizeh.ColorGradient("linear",((0,(0,.5,1)),(1,(0,1,1))), xy1=(-cosR,-sinR), xy2=(cosR,sinR))
 
-    gradRad1 = radius1 - 20
-    gradRad2 = radius1 + 20
-    gradient = gizeh.ColorGradient(type="radial",
-                                   stops_colors = [(0,color1),(1,color2)],
-                                   #xy1=[0.2,-0.2], xy2=[0.1,0.0], xy3 = [0.0,0.99])
-                                   xy1=[0.0,0.0], xy2=[gradRad1,0.0], xy3 = [0.0,gradRad2])
+    #gradRad1 = radius1 - 20
+    #gradRad2 = radius1 + 20
+    #gradient = gizeh.ColorGradient(type="radial",
+    #                               stops_colors = [(0,color1),(1,color2)],
+    #                               xy1=[0.0,0.0], xy2=[gradRad1,0.0], xy3 = [0.0,gradRad2])
     surface = gizeh.Surface(W,H)
 
-    #circle1 = gizeh.circle(r=1, xy = (0, 0), stroke=gradient, stroke_width=0.3).scale(50.0).translate(xy=(W/2, H/2))
-    #circle1 = gizeh.circle(radius1, xy = (W/2, H/2), fill=gradient)
-    circle1 = gizeh.circle(radius1, xy = (W/2, H/2), stroke=gradient, stroke_width=5)
-    circle1.draw(surface)
+    # orbit halo
+    #circle1 = gizeh.circle(radius1, xy = (W/2, H/2), stroke=gradient, stroke_width=5)
+    #circle1.draw(surface)
 
-    #circle4 = gizeh.circle(r=1, xy = (W/2,H/2), fill=color1)
-    #circle4.draw(surface)
+    for i in range(numParticles):
+      # Orbiting planet
+      particle = particles[i]
 
-    # Orbiting planet
-    circle3 = gizeh.circle(radius3, xy = (W/2 + cosR * radius1, H/2 + sinR * radius1), fill=(1,0,1))
-    circle3.draw(surface)
+      if (particle.easing == 1):
+        angle = pytweening.linear((duration - t) / duration) * 360 * particle.direction
+      elif (particle.easing == 2):
+        angle = pytweening.easeInQuad((duration - t) / duration) * 360 * particle.direction
+      elif (particle.easing == 3):
+        angle = pytweening.easeOutQuad((duration - t) / duration) * 360 * particle.direction
+      elif (particle.easing == 4):
+        angle = pytweening.easeInOutQuad((duration - t) / duration) * 360 * particle.direction
+      elif (particle.easing == 5):
+        angle = pytweening.easeInSine((duration - t) / duration) * 360 * particle.direction
+      elif (particle.easing == 6):
+        angle = pytweening.easeOutSine((duration - t) / duration) * 360 * particle.direction
+      elif (particle.easing == 7):
+        angle = pytweening.easeInOutSine((duration - t) / duration) * 360 * particle.direction
+      radians = math.radians(angle)
+      cosR = math.cos(radians)
+      sinR = math.sin(radians)
+      x = W/2 + cosR * particle.orbit_radius
+      y = H/2 + sinR * particle.orbit_radius
+      fill = particle.color
+      #circle = gizeh.circle(particle.radius, xy = (x, y), fill=(1,0,1))
+      circle = gizeh.circle(particle.radius, xy = (x, y), fill=fill)
+      circle.draw(surface)
 
     return surface.get_npimage()
 
+init()
 clip = mpy.VideoClip(make_frame, duration=duration)
-#clip.write_gif('myimage1.gif',fps=15, opt="OptimizePlus", fuzz=10)
-#clip.write_gif('myimage1.gif',fps=15, opt="OptimizePlus")
 clip.write_gif('myimage1.gif',fps=15)
 
 #new_clip = supersample(clip, d=0.05, nframes=5)
