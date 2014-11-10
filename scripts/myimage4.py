aa0b3213ae8e0e589813326698c5b730eccda067
Bradley Bossard <bradleybossard@gmail.com>
2014-11-10 15:47:31 -0800

    New pulsating balls animation

--- scripts/myimage4.py
+++ scripts/myimage4.py
@@ -1,11 +1,16 @@
-import random
+#import random
+#import numpy as np
+import pytweening
 import gizeh as gz
 import moviepy.editor as mpy
 
 # Width/Height, Duration, framerate
 W,H = 400, 400
-D = 3
+duration = 3
 fps = 25
+numBalls = 10
+radius = 10.0
+alpha = 1.0
 
 # Init function
 def init():
@@ -15,8 +20,24 @@ def init():
 def make_frame(t):
   surface = gz.Surface(W,H, bg_color=(1,1,1))
 
+  gradient = gz.ColorGradient(type="radial",
+                  stops_colors = [(0,(1,0,0, alpha)),(1,(0.9,0,0,alpha))],
+                  xy1=[0.3,-0.3], xy2=[0,0], xy3 = [0,1.4])
+  y = H / 2
+  for i in range(numBalls):
+    x = float(i) / numBalls * W
+    #newRadius = pytweening.easeInOutSine((duration - t) / duration) * radius
+    if (t < duration/2):
+    #newRadius = pytweening.easeInOutBounce((duration - t) / duration) * radius
+      newRadius = pytweening.easeInOutBounce(2 * t / duration) * radius
+    else:
+      newRadius = pytweening.easeInOutBounce(1 - (t / duration)) * radius
+
+    ball = gz.circle(r=newRadius, fill=gradient).translate((x, y))
+    ball.draw(surface)
+
   return surface.get_npimage()
 
 init()
-clip = mpy.VideoClip(make_frame, duration=D)
-clip.write_gif("gifs/myimage.gif", fps=fps, opt="OptimizePlus")
+clip = mpy.VideoClip(make_frame, duration=duration)
+clip.write_gif("gifs/myimage4.gif", fps=fps, opt="OptimizePlus")
